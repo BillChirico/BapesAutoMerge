29339c4d391cb874659c35ecdb6f0c47c1149a49
Bill Chirico <billchirico@gmail.com>
2023-01-31 13:50:16 -0800

    Complete refactor

--- src/BapesAutoMerge.lua
+++ src/BapesAutoMerge.lua
@@ -1,3 +1,5 @@
+local addonName, addonTable = ...;
+
 local MergeItems = {
 	[190322] = "Rousing Order",
 	[190315] = "Rousing Earth",
@@ -7,23 +9,33 @@ local MergeItems = {
 	[190326] = "Rousing Air"
 };
 
-local macroBtn = CreateFrame("Button", "myMacroButton", UIParent, "SecureActionButtonTemplate")
+local macroBtn = CreateFrame("Button", string.format("%s_MergeItemMacroButton", addonName), UIParent,
+	"SecureActionButtonTemplate")
+macroBtn:RegisterForClicks("AnyUp", "AnyDown")
 
-function MainFrame_OnEvent(self, event, ...)
-	print(event)
-	if event == "WORLD_MAP_OPEN" then
-		message("Open!")
+local f = CreateFrame("FRAME")
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("BAG_NEW_ITEMS_UPDATED")
+f:SetScript("OnEvent", function(self, event, ...)
+	if f[event] then
+		f[event](f, ...)
 	end
+end)
 
-	if event == "BAG_NEW_ITEMS_UPDATED" and not InCombatLockdown() then
-		macroBtn:RegisterForClicks("AnyUp", "AnyDown")
-		macroBtn:SetAttribute("macrotext1", "/raid zomg a left click!")
-		for k, v in pairs(MergeItems) do
-			local count = GetItemCount(k)
+function f:ADDON_LOADED(...)
+	if (...) == addonName then
+		print("BapesAutoMerge loaded! Feel free to contact me for any requests, feedback, or bugs at Bapes#1111 on Discord and Battle.net.")
+	end
+end
+
+function f:BAG_NEW_ITEMS_UPDATED(...)
+	if not InCombatLockdown() then
+		for itemID, itemName in pairs(MergeItems) do
+			local count = GetItemCount(itemID)
 
 			if count >= 10 then
-				macroBtn:SetAttribute("macrotext1", "/use " .. v)
-				message("Ready to merge " .. v .. " to Awakened")
+				macroBtn:SetAttribute("macrotext1", string.format("/use %s", itemName))
+				print(string.format("Ready to merge %s to Awakened", itemName))
 			end
 		end
 	end
