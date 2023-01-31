5bcdae9a7bafc2005c5aff48040a99a72504876b
Bill Chirico <billchirico@gmail.com>
2023-01-31 01:49:47 -0800

    Tests

--- src/MainFrame.lua
+++ src/MainFrame.lua
@@ -1,3 +1,23 @@
--- Author      : Bapes
--- Create Date : 1/30/2023 4:24:13 PM
+-- Author      : Bill Chirico
+-- Create Date : 1/31/2023 12:35:04 AM
 
+
+function MainFrame_OnLoad()
+	
+end
+
+function MainFrame_OnEvent(self, event, ...)
+	if(event == "WORLD_MAP_OPEN") then
+		message("Open!")
+	end
+end
+
+function SecureCmdUseItem(name, bag, slot, target)
+	if ( bag ) then
+		C_Container.UseContainerItem(bag, slot, target);
+	elseif ( slot ) then
+		UseInventoryItem(slot, target);
+	else
+		UseItemByName(name, target);
+	end
+end
